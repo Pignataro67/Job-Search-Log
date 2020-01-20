0db87929c969cb9a3ce8ab0b6274c75cb3ca6780
Pignataro67 <Pignataro67@gmail.com>
2020-01-20 13:23:38 -0800

    adding client and controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,15 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :update, :destroy]
 
-    # GET /tasks
   def index
     @tasks = Task.all  
     render json: @tasks
   end
   
-    # GET /tasks/1
   def show
     render json: @task
   end
-  
-    # POST /tasks
+ 
   def create
     @task = Task.new(task_params)
   
@@ -22,8 +19,7 @@ class TasksController < ApplicationController
       render json: @task.errors, status: :unprocessable_entity
     end
   end
-  
-    # PATCH/PUT /tasks/1
+
   def update
     if @task.update(task_params)
       render json: @task
@@ -31,19 +27,17 @@ class TasksController < ApplicationController
       render json: @task.errors, status: :unprocessable_entity
     end
   end
-  
-    # DELETE /tasks/1
+
   def destroy
     @task.destroy
   end
   
   private
-      # Use callbacks to share common setup or constraints between actions.
+
   def set_task
     @task = Task.find(params[:id])
   end
-  
-      # Only allow a trusted parameter "white list" through.
+
   def task_params
     params.fetch(:task, {})
   end
