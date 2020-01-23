d308776e09992132fb4ed44ea192efe0b59c08a2
Pignataro67 <Pignataro67@gmail.com>
2020-01-23 12:42:42 -0800

    progress getting server up

--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import { Provider } from 'react-redux';
-// import RootReducer from './reducers/index';
-// import './index.css';
-// import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import RootReducer from './reducers/index'; 
+import './index.css';
+import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-// const store = createStore(
-//     RootReducer,
-//     compose(
-//         applyMiddleware(thunk),
-//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )
-//   );
+const store = createStore(
+    RootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
   
-//   class Hello extends React.Component {
-//     render() {return <div>Hello!</div>}
-//   }
-//   class Goodbye extends React.Component {
-//     render() {return <div>Goodbye!</div>}
-//   }
-//   ReactDOM.render(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>,
-//     document.getElementById('root'));
+  class Hello extends React.Component {
+    render() {return <div>Hello!</div>}
+  }
+  class Goodbye extends React.Component {
+    render() {return <div>Goodbye!</div>}
+  }
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
