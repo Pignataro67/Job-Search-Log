e091ab99cd58f6a6d46e0b938cc375fc71d258fe
Pignataro67 <Pignataro67@gmail.com>
2020-01-27 12:46:44 -0800

    adding case in reducer

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     # get '/types', to: 'types#index', as: 'types'
     # get '/types/:id', to: 'types#show', as: 'type'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
