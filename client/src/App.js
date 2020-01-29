87573aec13768cdbb141732dbb515af021619cc0
Pignataro67 <Pignataro67@gmail.com>
2020-01-29 11:39:17 -0800

    changing cont. to idx

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import TasksContainer from './containers/tasks_container';
+import TasksIndex from './containers/tasks_index';
 import TasksNew from './containers/tasks_new';
 
 class Hello extends Component {
@@ -14,7 +14,7 @@ class App extends Component {
       <Router>
         <div className="app">
           <Switch>
-            <Route path="/tasks/new" component={ TasksNew } />
+            <Route path="/tasks/new" component={ TasksIndex } />
             <Route path="/" component={ TasksContainer } />
           </Switch>  
         </div>
