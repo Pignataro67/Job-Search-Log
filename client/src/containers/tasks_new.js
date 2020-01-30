52fb95a405064c9a6dc043b4631d18a44be0ae17
Pignataro67 <Pignataro67@gmail.com>
2020-01-30 12:44:48 -0800

    adding tasks new with redux form

--- client/src/containers/tasks_new.js
+++ client/src/containers/tasks_new.js
@@ -1,14 +1,65 @@
 import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class TasksNew extends Component {
 
-  render() {
+  renderField(field){
+    const { meta } = field;
+    const className = `form-group ${meta.touched && meta.error ? 'has-danger': '' }`;
     return (
-      <div>
-        "NEW TASK"
+      <div className={className}>  
+        <input
+          placeholder={field.label}
+          className="form-control"
+          type="text"
+          {...field.input}   
+        />
+        <div className="text-help">
+          {meta.touched ? meta.error : ''}
+        </div>
       </div>
+    )
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+  
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Name"
+          name="name"
+          component={this.renderField}
+        />
+        <Field
+          label="Description"
+          name="description"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Save</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
     )
   }
 }
 
-export default TasksNew;
+function validate(values){
+const errors = {};
+
+  if (!values.name) {
+    errors.name = "Enter a name!";
+  }
+  if (!values.description) {
+    errors.description = "Enter a description!";
+  }
+
+  return errors;
+}
+
+export default reduxForm({ validate, form: 'TasksNewForm' })(TasksNew);
