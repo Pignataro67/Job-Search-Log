be0756406ba665cbbaab3e4b4167a6d1954a617c
Pignataro67 <Pignataro67@gmail.com>
2020-01-31 12:24:52 -0800

    button spacing and createTask func.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,8 @@ import fetch from 'isomorphic-fetch';
 require('es6-promise').polyfill();
 export const FETCH_TASKS = 'FETCH_TASKS';
 export const RECEIVED_TASKS = 'RECEIVED_TASKS';
-
-const ROOT_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+export const CREATE_TASK = 'CREATE_TASK';
+const ROOT_URL = 'http://localhost:3001/api';
 
 export function fetchTasks() {
   return (dispatch) => {
@@ -15,4 +15,18 @@ export function fetchTasks() {
         dispatch({type: RECEIVED_TASKS, payload: json})
       })
   };
+}
+
+export function createTask(values) {
+  return (dispatch) => {
+    fetch(`${ROOT_URL}/tasks`, {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+        .then(json => dispatch({ type: CREATE_TASK, payload: json }))
+  }
 }
