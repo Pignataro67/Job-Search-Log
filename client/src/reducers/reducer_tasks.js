f26396f23387a0d45c1f3234905114b921b11125
Pignataro67 <Pignataro67@gmail.com>
2020-02-05 12:30:52 -0800

    adding dependencies, reducer

--- client/src/reducers/reducer_tasks.js
+++ client/src/reducers/reducer_tasks.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_TASK, RECEIVED_TASK, FETCH_TASKS, RECEIVED_TASKS, DELETE_TASK } from '../actions';
+import { FETCH_TASK, RECEIVED_TASK, FETCH_TASKS, RECEIVED_TASKS, DELETE_TASK, CREATE_TASK } from '../actions';
 
 export default function(state = {}, action) {
 
@@ -12,6 +12,8 @@ export default function(state = {}, action) {
       return {...state}  
     case RECEIVED_TASKS:
       return  _.mapKeys(action.payload, 'id')
+    case CREATE_TASK:
+      return {...state, [action.payload.id]: [action.payload][0] }
     case DELETE_TASK:
       return _.omit(state, action.payload)
     default:
